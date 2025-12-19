0999d93c2d8e0f8547fef5ae5326b0def5466ecd
Lee you na <younalee6262@gmail.com>
2025-12-19 10:38:28 +0900

    Netlify 배포 준비 완료

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="card-news-container">
+        <h1>카드 뉴스</h1>
+        <div className="card-news-content">
+          <p>콘텐츠가 여기에 표시됩니다.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
